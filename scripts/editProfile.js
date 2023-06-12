35a3fa462f6582f91f49688db4372483f71efb15
Patricio Sala <patricio.sala.ext@errepar.com>
2023-06-11 22:25:22 -0300

    blur header for safari

--- scripts/editProfile.js
+++ scripts/editProfile.js
@@ -1,18 +1,27 @@
 let editProfileDialog = document.querySelector('.edit-profile-dialog');
 
-
 let editProfileBtn = document.querySelector('.edit-profile-btn');
 let closeEditProfileBtn = document.querySelector('.close-btn');
 
 let editBannerContainer = document.querySelector('.edit-banner-container');
 let editAvatarContainer = document.querySelector('.edit-profile-avatar');
+let editUsername = document.querySelector('#username');
+let editBio = document.querySelector('#bio');
+
+let bannerUrl = session.banner_url;
+let avatarUrl = session.avatar_url;
+let username = session.username;
+let bio = session.bio;
 
 let openEditProfileDialog = () => {
     editProfileDialog.showModal();
-    editBannerContainer.style.backgroundImage = `url(${session.banner_url})`;
-    editAvatarContainer.style.backgroundImage = `url(${session.avatar_url})`;
+    editBannerContainer.style.backgroundImage = `url(${bannerUrl})`;
+    editAvatarContainer.style.backgroundImage = `url(${avatarUrl})`;
+    editUsername.value = username;
+    editBio.value = bio;
 }
 
 let closeEditProfileDialog = () => {
     editProfileDialog.close();
 }
+
